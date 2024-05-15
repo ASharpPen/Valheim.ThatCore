f78f0ea4139e45efdd652eb30b1fae8be1c7563c
Asharppen <asharppen@gmail.com>
2024-05-15 20:17:02 +0200

    Fixed biome enum splitting for valheim v0.218.15

--- src/ThatCore.Valheim/Extensions/HeightmapBiomeExtensions.cs
+++ src/ThatCore.Valheim/Extensions/HeightmapBiomeExtensions.cs
@@ -11,7 +11,14 @@ public static class HeightmapBiomeExtensions
 
         foreach (Heightmap.Biome value in Enum.GetValues(typeof(Heightmap.Biome)))
         {
-            if ((biomeMask & value) > 0)
+            if (value == Heightmap.Biome.All)
+            {
+                if (biomeMask == Heightmap.Biome.All)
+                {
+                    result.Add(value);
+                }
+            }
+            else if ((biomeMask & value) > 0)
             {
                 result.Add(value);
             }
